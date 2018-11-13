3ab0e7f69c758127b3275417a45d5e84ce42ca61
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-13 14:40:19 +0900

    Prepare development environment using vagrant

---
+++ misc/provision.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install build-essential rake bison git gperf automake m4 \
+                autoconf libtool cmake pkg-config libcunit1-dev ragel
+sudo apt-get -y upgrade
+
+curl https://sh.rustup.rs -sSf > rustup.sh
+sh rustup.sh -y
+source $HOME/.cargo/env
+rustup update
+rustup install nightly
+rustup component add rustfmt-preview --toolchain nightly
+rustup default nightly
+
+git clone https://github.com/matsumotory/middlecon
+cd middlecon && cargo build
